6cb3294a3df477a5e1806471e55eb85b6adca393
Gojko Adzic <gojko@gojko.com>
2016-10-28 23:26:20 +0100

    dynamic link works

--- recompile-dynamic.sh
+++ recompile-dynamic.sh
@@ -1,15 +1,11 @@
 sudo yum install shared-mime-info cairo cairo-devel gobject-introspection gobject-introspection-devel giflib-devel libjpeg-turbo-devel poppler-glib-devel pango-devel harfbuzz-devel -y
 
 export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/lib/pkgconfig
-#sudo yum install rpm-build -y
-#curl -OL ftp://ftp.redhat.com/pub/redhat/linux/enterprise/6Server/en/os/SRPMS/libcroco-0.6.2-5.el6.src.rpm
-#rpmbuild --rebuild libcroco-0.6.2-5.el6.src.rpm 
-#sudo rpm -i libcroco-0.6.2-5.el6.src.rpm
 
 curl -OL http://ftp.gnome.org/pub/GNOME/sources/libcroco/0.6/libcroco-0.6.8.tar.xz
 tar xf libcroco-0.6.8.tar.xz 
 cd libcroco-0.6.8
-./configure --prefix=usr && make && sudo make install && sudo ldconfig && cd ..
+./configure --prefix=/usr && make && sudo make install && sudo ldconfig && cd ..
 
 curl -OL http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.28/gdk-pixbuf-2.28.2.tar.xz 
 tar xf gdk-pixbuf-2.28.2.tar.xz 
@@ -20,5 +16,5 @@ make && sudo make install && sudo ldconfig && sudo gdk-pixbuf-query-loaders --up
 curl -OL http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.26/librsvg-2.26.3.tar.gz
 tar xf librsvg-2.26.3.tar.gz 
 cd librsvg-2.26.3
-./configure --prefix=usr && make && sudo make install && sudo ldconfig && cd ..
+./configure --prefix=/usr && make && sudo make install && sudo ldconfig && cd ..
 
