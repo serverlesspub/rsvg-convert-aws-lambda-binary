d98667620f91b861d5e0ffdd82a73e42322af78a
Gojko Adzic <gojko@gojko.com>
2016-10-28 22:52:08 +0100

    svg

--- recompile.sh
+++ recompile.sh
@@ -1,7 +1,7 @@
 
 BUILD_DIR=`pwd`/build
 export XDG_DATA_DIRS=$BUILD_DIR/share/
-export PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig
+export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/lib/pkgconfig
 export PATH=$BUILD_DIR/bin:/usr/bin:$PATH
 export LDFLAGS=-L$BUILD_DIR/lib:/usr/lib64:/usr/lib
 export LD_LIBRARY_PATH=$BUILD_DIR/lib:/usr/lib64:/usr/lib
@@ -60,7 +60,7 @@ rebuild http://ftp.gnome.org/pub/GNOME/sources/libcroco/0.6/libcroco-0.6.8.tar.x
 rebuild https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.20.tar.bz2 harfbuzz
 rebuild http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.10.91.tar.gz fontconfig --enable-libxml2 --disable-docs
 rebuild https://www.cairographics.org/releases/pixman-0.34.0.tar.gz pixman
-rebuild http://cairographics.org/releases/cairo-1.14.6.tar.xz cairo
+rebuild http://cairographics.org/releases/cairo-1.14.6.tar.xz cairo --enable-xml
 rebuild http://ftp.gnome.org/pub/GNOME/sources/pango/1.34/pango-1.34.1.tar.xz pango
 rebuild http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.28/gdk-pixbuf-2.28.2.tar.xz gdk-pixbuf --disable-modules
 rebuild http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.40/librsvg-2.40.6.tar.xz librsvg --disable-introspection
